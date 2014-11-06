25e3c8a0ec8a8576aa2abd8d4965c43889c5fbee
Maurice Bodlund <maurice.bodlund.win14@utb.hermods.se>
2014-11-06 15:06:16 +0100

    yes!
    Take2

---
+++ ChessProject2/ChessProject2/ChessPieces.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessProject2
+{
+    class ChessPieces
+    {
+        public string Color { get; set; }
+
+        public int PositionX { get; set; }
+
+        public int PositionY { get; set; }
+
+
+    }
+
+    class Pawn : ChessPieces
+    {
+        class Pawn (string Colour)  
+        {
+             
+        }
+    }
+}
---
+++ ChessProject2/ChessProject2/GameBrain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessProject2
+{
+    class GameBrain
+    {
+        
+        public void TryingHard()
+        {
+            string[,] board = new string[8,8];
+            int y = 0;
+            for (int x = 0; x <=7; x++)
+            {
+                board[x, y] = "[ ]";
+                for ( y = 0; y <=7; y++)
+                {
+                    board[x, y] = "y";    
+                }
+            }
+        }
+    }
+}
